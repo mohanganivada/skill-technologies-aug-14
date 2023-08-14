23675e78e2535f94056a736fd0677a473c66e049
mohan_ganivada <ganivasniz20852@gmail.com>
2023-08-14 13:13:27 +0530

    first

--- skill/settings.py
+++ skill/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'skill.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'skill',
-        'USER': 'root',
-        'PASSWORD': 'Mohan3658',
-        'HOST': 'localhost',  
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'skill',
+#         'USER': 'root',
+#         'PASSWORD': 'Mohan3658',
+#         'HOST': 'localhost',  
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
