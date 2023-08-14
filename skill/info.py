9018f672a5fbb34e0ea9a30e4e794b7263a2222f
mohan_ganivada <ganivasniz20852@gmail.com>
2023-08-14 12:59:51 +0530

    first

--- skill/info.py
+++ skill/info.py
@@ -1,6 +1,6 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kalyan.k@skilltechnologies.in'
-EMAIL_HOST_PASSWORD = 'ucgwtadusajtrdys'
+EMAIL_HOST_USER = 'ganivasniz20852@gmail.com'
+EMAIL_HOST_PASSWORD = 'cdaivibxbojvfqop'
 EMAIL_PORT = 587
-SITE_ADMIN_EMAIL='kalyan.k@skilltechnologies.in'
+SITE_ADMIN_EMAIL='ganivasniz20852@gmail.com'
